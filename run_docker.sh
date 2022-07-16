a1d6eaddf27398c448bf3c5336d324ddb53d0259
EC2 Default User <ec2-user@ip-172-31-44-80.ec2.internal>
2022-07-16 17:48:11 +0000

    run_docker update

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=prediction-api .
+docker build --tag=ml-api .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker images
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 prediction-api
+docker run -p 8000:80 ml-api
